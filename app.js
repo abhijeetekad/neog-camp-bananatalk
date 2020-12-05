76812b767ef0925a82865a1651a2e1627322da59
abhijeetekad <70329145+abhijeetekad@users.noreply.github.com>
2020-12-05 16:06:15 +0530

    code

--- app.js
+++ app.js
@@ -1,10 +1,33 @@
-var btnTranslate = document.querySelector('#btn-translate')
-var txtInput = document.querySelector('#txt-Input')
+var btnTranslate = document.querySelector('#btn-translate');
+var txtInput = document.querySelector('#txt-Input');
 var outputDiv = document.querySelector("#output"); 
 
-console.log(outputDiv)
+var serverURL = "https://api.funtranslations.com/translate/minion.json"
+
+function getTranslationURL(input) {
+    return serverURL  + "?" +"text" + input
+}
+
+function errorHandler(error) {
+    console.log("error occured", error);
+    alert("something wrong with server! try again after some time")
+}
+
+
 
 function clickEventHandler(){
-    outputDiv.innerText = "kcdacvacd " + txtInput.value;
+    var inputText = txtInput.Value;
+
+
+    fetch(getTranslationURL(inputText))
+       .then(response => response.json())
+       .then(json =>{
+          var translatedText = json.contents.translated;
+          outputDiv.innerText =translatedText;
+          })
+       .catch(errorHandler)
 };
-btnTranslate.addEventListener("click",clickEventHandler)
+
+
+
+btnTranslate.addEventListener("click",clickHandler)
