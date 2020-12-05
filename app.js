533dcf3dd1f0c365080abbd13b37905d6c5816c3
abhijeetekad <70329145+abhijeetekad@users.noreply.github.com>
2020-12-05 16:26:31 +0530

    some code

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
-var btnTranslate = document.querySelector('#btn-translate');
-var txtInput = document.querySelector('#txt-Input');
-var outputDiv = document.querySelector("#output"); 
+var btnTranslate = document.querySelector("#btn-translate");
+var txtInput = document.querySelector("#txt-input");
+var outputDiv = document.querySelector("#output");
+
+// var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
 
 var serverURL = "https://api.funtranslations.com/translate/minion.json"
 
+
 function getTranslationURL(input) {
-    return serverURL  + "?" +"text" + input
+    return serverURL + "?" + "text=" + input
 }
 
 function errorHandler(error) {
@@ -14,20 +17,17 @@ function errorHandler(error) {
 }
 
 
+function clickHandler() {
+    var inputText = txtInput.value; // taking input
 
-function clickEventHandler(){
-    var inputText = txtInput.Value;
-
-
+    // calling server for processing
     fetch(getTranslationURL(inputText))
-       .then(response => response.json())
-       .then(json =>{
-          var translatedText = json.contents.translated;
-          outputDiv.innerText =translatedText;
-          })
-       .catch(errorHandler)
+        .then(response => response.json())
+        .then(json => {
+            var translatedText = json.contents.translated;
+            outputDiv.innerText = translatedText; // output
+           })
+        .catch(errorHandler)
 };
 
-
-
-btnTranslate.addEventListener("click",clickHandler)
+btnTranslate.addEventListener("click", clickHandler)
